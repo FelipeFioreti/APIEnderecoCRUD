abf1cf4fdbbf73be8a707c976b74002fa1b0f761
FelipeFioreti <felipe.fioretirosolen@gmail.com>
2025-04-26 00:29:42 -0300

    Adicionando mudanças no Front-End

--- ConsumoAPI/assets/script/script.js
+++ ConsumoAPI/assets/script/script.js
@@ -1,24 +1,9 @@
-
-/*
-const getCEP = async () => {
-    const cep = document.getElementById("cep").value; 
-    
-    if(cep.len !== 8){
-        alert("CEP inválido. Digite um CEP com 8 digítos.");
-        return;
-    }
-
-    const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
-    return response.json;
-}
-*/
-
-
 function buscarCEP() {
-    const cep = document.getElementById("cep").value; 
+    
     const retorno = document.getElementById("retorno").style;
     const botaoCadastro = document.getElementById("btn-cadastrar").style;
     const classeBotao = document.querySelectorAll(".btn");
+    const cep = document.getElementById("cep").value; 
 
     if(cep.length !== 8){
         alert("CEP inválido. Digite um CEP com 8 digítos.");
@@ -50,6 +35,19 @@ function buscarCEP() {
         elemento.style.width = "40%";
         elemento.style.height = "80px";
     });
+}
+
+function cadastrarEndereco() {
+
+    const cep = document.getElementById("cep").value; 
     
+    fetch(`https://viacep.com.br/ws/${cep}/json/`).then(response => {
+        if(!response.ok) {
+            console.log("Erro na requisição:", response.statusText);
+            throw new Error("Erro na requisição");
+        }
+
+        alert("Endereço cadastrado com sucesso!");
+    });
 }
 
